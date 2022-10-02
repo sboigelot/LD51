023f1c926a7c1f66a0ffd65ceaf366cf5dc63b2c
Simon Boigelot <sboigelot@gmail.com>
2022-10-02 14:19:32 +0200

    Scan screen working

--- LD51/Scenes/Supermarket.gd
+++ LD51/Scenes/Supermarket.gd
@@ -1,21 +1,27 @@
 extends Node2D
+class_name Supermarket2d
 
 signal rotate_package(add_rotation)
 
 export(NodePath) var hand_node_np
-onready var hand_node = get_node(hand_node_np) as Node2D
-export(float) var hand_min_y
-
 export(NodePath) var scanner_np
-onready var scanner = get_node(scanner_np) as Sprite
 export(NodePath) var scanner_texturerect_np
-onready var scanner_texturerect = get_node(scanner_texturerect_np) as TextureRect
+export(NodePath) var scanner_screen_center_np
 export(NodePath) var scanner_laser_np
-onready var scanner_laser = get_node(scanner_laser_np) as Line2D
 export(NodePath) var scanner_anim_np
-onready var scanner_anim = get_node(scanner_anim_np) as AnimationPlayer
 export(NodePath) var scanner_on_charger_np
+export(NodePath) var scanner_test_label_np
+
+onready var hand_node = get_node(hand_node_np) as Node2D
+onready var scanner = get_node(scanner_np) as Sprite
+onready var scanner_texturerect = get_node(scanner_texturerect_np) as TextureRect
+onready var scanner_screen_center = get_node(scanner_screen_center_np) as Position2D
+onready var scanner_laser = get_node(scanner_laser_np) as Line2D
+onready var scanner_anim = get_node(scanner_anim_np) as AnimationPlayer
 onready var scanner_on_charger = get_node(scanner_on_charger_np) as Sprite
+onready var scanner_test_label = get_node(scanner_test_label_np) as Label
+
+export(float) var hand_min_y
 
 export(bool) var scanner_in_hands setget set_scanner_hands, get_scanner_hands
 func set_scanner_hands(value:bool):
@@ -31,7 +37,9 @@ func set_viewport_texture(viewport:Viewport):
 	scanner_texturerect.texture = texture
 
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	set_scanner_hands(false)
+#	set_viewport_texture()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
@@ -44,8 +52,10 @@ func _process(delta):
 		_on_RightButton_pressed()
 
 	hand_node.position = get_global_mouse_position()
+#	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	if hand_node.position.y < hand_min_y:
 		hand_node.position.y = hand_min_y
+#		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if get_scanner_hands():
--- LD51/Scenes/Supermarket3d.gd
+++ LD51/Scenes/Supermarket3d.gd
@@ -5,31 +5,37 @@ class_name Supermarket3d
 var scan_package: Package
 
 export(NodePath) var scan_package_holder_np
-onready var scan_package_holder = get_node(scan_package_holder_np) as Spatial
-export(Vector3) var package_scale_on_belt = Vector3(0.75,0.75,0.75)
-export(float) var conveyor_speed = 60.0
+export(NodePath) var supermarket2d_np
 
-const package_scene = preload("res://Scenes/Package.tscn")
+onready var scan_package_holder = get_node(scan_package_holder_np) as Spatial
+onready var supermarket2d = get_node(supermarket2d_np) as Supermarket2d
+
+export(Vector3) var package_scale_on_belt = Vector3(0.75,0.75,0.75)
 
 onready var camera = $Camera as Camera
 onready var scan_camera = $ScanViewport/ScanCamera as Camera
 
+export(float) var conveyor_speed = 60.0
+
+const package_scene = preload("res://Scenes/Package.tscn")
+
+
 func _on_Supermarket_rotate_package(add_rotation):
 	rotate_package(add_rotation)
-	
+
 func rotate_package(add_rotation: Vector2):
 	if scan_package == null:
 		return
 	scan_package.add_to_target_rotation(add_rotation)
-	
+
 func _ready():
 	if scan_package_holder.get_child_count() >= 1:
 		scan_package = scan_package_holder.get_child(0)
 	spawn_package()
-	
+
 func _on_Timer_timeout():
 	spawn_package()
-	
+
 func spawn_package():
 	var instance = package_scene.instance()
 	var spawn_positions = [
@@ -40,12 +46,12 @@ func spawn_package():
 	]
 	instance.transform = spawn_positions[randi() % spawn_positions.size()]
 	instance.connect("clicked", self, "on_conveyor_belt_package_clicked")
-	
+
 #	Rotate randomly
 	var axis = Vector3.UP
 	var rotation_radiant = deg2rad(randi() % 360)
 	instance.transform.basis = instance.transform.basis.rotated(axis, rotation_radiant)
-	
+
 #	scale
 	instance.scale = package_scale_on_belt
 	$ConveyorBelt.add_child(instance)
@@ -62,7 +68,7 @@ func _physics_process(delta):
 
 	var direction = Vector3.RIGHT
 	var velocity = direction * conveyor_speed
-		
+
 	for child in $ConveyorBelt.get_children():
 		if child is Package:
 			var package = child as Package
@@ -70,16 +76,39 @@ func _physics_process(delta):
 
 func _process(delta):
 	move_scan_camera()
-	
+
 func move_scan_camera():
-	var x_extremes = [ -9, +9]
-	var x_range = 18.0
-	var y_extremes = [-5, +5]
-	var y_range = 10.0
+	var screen_extend = Vector2(8.65,4.75)
+	var cam_translation_range = screen_extend * 2
+	
 	var mouse_postion = get_viewport().get_mouse_position() as Vector2
+	var mouse_screen_percentage = Vector2(
+		mouse_postion.x / OS.window_size.x,
+		mouse_postion.y / OS.window_size.y
+	)
+	
+	var scanner_position = supermarket2d.scanner_screen_center.global_position
+	var scanner_position_screen_percentage = Vector2(
+		scanner_position.x / OS.window_size.x,
+		scanner_position.y / OS.window_size.y
+	)
 
-	scan_camera.translation.x = mouse_postion.x / OS.window_size.x * x_range - x_range /2
-	scan_camera.translation.z = mouse_postion.y / OS.window_size.y * y_range - y_range /2
+#	good but under hand not under scanner
+#	scan_camera.translation.x = (mouse_screen_percentage.x * cam_translation_range.x) - screen_extend.x
+#	scan_camera.translation.z = (mouse_screen_percentage.y * cam_translation_range.y) - screen_extend.y
 
-#	scan_camera.translation.x = mouse_postion.x / OS.window_size.x * (camera.size / 1) - (camera.size)
-#	scan_camera.translation.z =  mouse_postion.y # / OS.window_size.y * (camera.size / 2) - (camera.size / 2)
+	scan_camera.translation.x = (scanner_position_screen_percentage.x * cam_translation_range.x) - screen_extend.x
+	scan_camera.translation.z = (scanner_position_screen_percentage.y * cam_translation_range.y) - screen_extend.y
+
+	supermarket2d.scanner_test_label.text = ("mouse%%:\n\tx: %s\n\ty: %s\n"%[
+		round(mouse_screen_percentage.x*100),
+		round(mouse_screen_percentage.y*100)
+	] +
+	"screen center%%:\n\tx: %s\n\ty: %s\n"%[
+		round(scanner_position_screen_percentage.x*100),
+		round(scanner_position_screen_percentage.y*100)
+	] +
+	"cam translation:\n\tx: %s\n\tzx: %s\n"%[
+		scan_camera.translation.x,
+		scan_camera.translation.z
+	])
